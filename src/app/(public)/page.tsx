4cf2d36b67514c96f3256fd1b57be469294068ba
Priscilla Haibara <megumi.ufrn@gmail.com>
2026-03-15 15:23:13 -0300

    feat: add Hero component structure

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from "@/components/layout/Header";
+import { Hero } from "@/components/layout/Hero";
 
 export default function Home() {
   return (
-    <div>
+    <div className="min-h-screen">
       <Header />
+      <Hero />
     </div>
   );
 }
